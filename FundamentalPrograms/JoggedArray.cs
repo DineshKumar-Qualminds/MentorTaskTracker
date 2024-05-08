33c1b15a6965d9822006b3032ed155c25e107be8
Dinesh Kumar Nadakuduru <ndkumar@qualminds.com>
2024-05-08 13:53:18 +0530

    add changes

---
+++ FundamentalPrograms/JoggedArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentalPrograms
+{
+    internal class JoggedArray
+    {
+       static void Main()
+        {
+            int[][] array = new int[2][];
+
+            array[0] = [4, 6, 1, 8, 6, 7];
+            array[1] = [3, 6, 2, 4];
+
+
+            for (int i=0; i<array.Length; i++)
+            {
+                Console.Write($"Elements ({i}) :");
+             
+
+                for(int j=0; j < array[i].Length; j++)
+                {
+                    Console.Write("{0}{1}",array[i][j], j == (array[i].Length-1) ? "": " ");
+                  
+                }
+                Console.WriteLine();
+
+            }  
+        }
+    }
+}
---
+++ FundamentalPrograms/LeftAngleTriangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentalPrograms
+{
+    class LeftAngleTriangle
+    {
+     static void Main()
+        {
+           
+            int x;
+            do
+            {
+                Console.Write("Enter value based on size of triangle depends : ");
+             
+                if (!int.TryParse(Console.ReadLine(), out x))
+                {
+                    Console.WriteLine("Invalid Input Value,Enter Valid Number");
+                }
+
+            }while(x==0);
+            Console.WriteLine("\n");
+
+            for (int i = 1; i <=x; i++)
+            {
+               for(int j = 1; j <= x-i; j++)
+                {
+                    Console.Write(" ");
+                }
+               for(int k = 1; k <=i; k++)
+                {
+                    Console.Write("*");
+                }
+               Console.WriteLine();
+            
+            }
+          
+        }
+       
+    }
+}
