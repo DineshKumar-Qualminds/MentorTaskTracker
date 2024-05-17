7b5bb1d116e7460ae0f9f38233d7e718ab3d1936
Dinesh Kumar Nadakuduru <ndkumar@qualminds.com>
2024-05-17 12:53:49 +0530

    Generics and ADO Concepts

---
+++ PaginationUsingDS&CB/DisplayEmployeeList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaginationUsingADO
+{
+    public class DisplayEmployeeList
+    {
+        public static void DisplayEmployees(List<Employee> employees, int pageNumber, int pageSize)
+        {
+            int startIndex = (pageNumber -1) * pageSize;    
+            int endIndex = Math.Min(startIndex+pageSize-1,employees.Count-1);
+
+            Console.WriteLine("------------------------------------------------------------------------------------------------");
+            Console.WriteLine("|    Id    |   First Name   |   Last Name   |   Date of Birth   |             Email          |");
+            Console.WriteLine("-------------------------------------------------------------------------------------------------");
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                Console.WriteLine($"| {employees[i].Id,-7} | {employees[i].FirstName,-14} | {employees[i].LastName,-13} | " +
+                    $"{employees[i].DateOfBirth.ToShortDateString(),-17} | {employees[i].EmailId,-20} |");
+            }
+
+            Console.WriteLine("-----------------------------------------------------------------------------");
+         
+        }
+        
+    }
+}
